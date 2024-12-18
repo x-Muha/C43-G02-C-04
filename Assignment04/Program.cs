d6340bec831c75082ca10ad13dce4d34d2457490
x-Muha <muhmdusam4@gmail.com>
2024-12-18 22:01:19 +0200

    Added The rest of questions till Q20

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Channels;
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Threading.Channels;
 
 namespace Assignment04
 {
@@ -198,6 +200,138 @@ namespace Assignment04
             //foreach (int x in arr) Console.Write($"{x} ");
             #endregion
 
+            #region Q14
+            //int[] arr = { 1, 3, 5, 7, 1, 41, 32, 12, 1, 4, 6, 3 };
+            //int[] counts = new int[arr.Length];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (counts[i]== -1) continue;
+            //    int j = i;
+            //    while (j < arr.Length)
+            //    {
+            //        if (arr[j] == arr[i]) {
+            //            counts[i]++;
+            //            if(i!=j) counts[j] = -1;
+            //            }
+            //        j++;
+            //    }
+            //}
+            //for (int i = 0; i <= arr.Length; i++)
+            //{
+            //    if(counts[i]==-1) continue;
+            //    Console.WriteLine($"count of number \"{arr[i]}\" is: {counts[i]}");
+            //}
+
+            #endregion
+
+            #region Q15
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            //int max = arr[0];
+            //int min = arr[0];
+
+            //foreach (int i in arr)
+            //{
+            //    if (i > max) max = i;
+            //    if (i < min) min = i;
+            //}
+            //Console.WriteLine($"Max = {max}, Min = {min}");
+            #endregion
+
+            #region Q16
+
+            //int[] arr = { 1, 2, 7, 4, 9, 15, 12, 5, 6, 7, 8, 9, 10, 11 };
+
+            //Array.Sort(arr);
+            //Array.Reverse(arr);
+            //Console.WriteLine($"Second largest element is: {arr[1]}");
+
+            #endregion
+
+            #region Q17
+            ////          Creation and filling of array:
+            //int n=0;
+            //bool isParsed = false;
+            //Console.Write("please enter the size of array: ");
+            //while (!isParsed) isParsed = int.TryParse(Console.ReadLine(), out n);
+            //int[] arr = new int[n];
+            //Console.WriteLine("Enter Valuse of array:-");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    isParsed = false ;
+            //    while(!isParsed) isParsed = int.TryParse(Console.ReadLine() , out arr[i]);
+            //}
+            ////          7,0,0,0,0,5,6,7,5,0,7,5,3
+            ////          Longest distance calculatoin
+            //int d,longestD =0;
+            //int[] isCalculated = new int[n];
+            //for (int i = 0;i < n; i++)
+            //{
+            //    if (isCalculated[i] == 1) continue;
+            //    d = 0;
+            //    for (int j = i+1; j < n; j++)
+            //    {
+            //        if ((arr[j] == arr[i])) d = j - i -1;
+            //        isCalculated[j] = 1;
+            //    }
+            //        if (d> longestD) longestD = d;
+            //}
+            //Console.WriteLine($"Longest distance = {longestD}");
+
+
+            #endregion
+
+            #region Q18
+
+
+            //String str = "this is a test";
+
+            //String[] strings = str.Split(' ');
+            //for (int i = strings.Length-1; i >=0; i--)
+            //    Console.Write($"{strings[i]} ");
+
+
+            #endregion
+
+            #region Q19
+            //int n = 0;
+            //bool isParsed = false;
+            //while (!isParsed) isParsed = int.TryParse(Console.ReadLine(), out n);
+
+            //int[,] arr1 = new int[n,n];
+            //int[,] arr2 = new int[n,n];
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"Enter elements for Row number {i+1}: ");
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        isParsed = false;
+            //        while (!isParsed) isParsed = int.TryParse(Console.ReadLine(), out arr1[i,j]);
+            //    }
+            //}
+            //for (int i = 0; i < n; i++)
+            //    for (int j = 0; j < n; j++)
+            //        arr2[i, j] =arr1[i, j];
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.Write($"{arr2[i, j]}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region Q20
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Array.Reverse(arr);
+            //foreach (int i in arr)
+            //    Console.Write($"{i}, ");
+
+            #endregion
         }
     }
 }
